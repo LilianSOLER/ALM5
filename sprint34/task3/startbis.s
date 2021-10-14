79e6ee46b1645f1e8b7a1814710544b03b3bc001
Lilian Soler <soler.lilian64@gmail.com>
2021-10-14 12:55:30 +0200

    Modification task3 pour séparer la fonction somme

--- sprint34/task3/startbis.s
+++ sprint34/task3/startbis.s
@@ -10,22 +10,9 @@
 .text @zone des instructions
 	_start:
 		mov sp, #0xFF
-		b _L1
 		.word array1
-
-	_L1:
-		mov r1, #0 @sommme
-		mov r2, #0 @élément du tableau
 		ldr r3 , =array1 @début du tableau$
-		b _loop
-
-	_loop:
-		ldr r2, [r3] @lire l'élément du tableau
-		cmp r2, #0 @si élément = 0
-		beq _halt @deuxième cas
-		add r1, r2, r1 @somme += élément du tableau
-		add r3, #4 @adresse suivante
-		b _loop @continuer
+		bl _sum
 
 	_halt:
 		mov r0, #0x4 @afficher bye
